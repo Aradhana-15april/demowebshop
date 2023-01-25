fa137cdd2da9b377f4ef4cebf37514dc83e2cde0
Aradhana <aradhanaabraham680@gmail.com>
2023-01-25 09:42:52 +0530

    file committed

---
+++ src/main/java/com/demowebshop/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package com.demowebshop.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    public WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}//
---
+++ src/test/java/com/demowebshop/testscript/HomeTest.java
@@ -0,0 +1,9 @@
+package com.demowebshop.testscript;
+
+import com.demowebshop.automationcore.Base;
+
+import java.util.Base64;
+
+public class HomeTest extends Base {
+
+}
